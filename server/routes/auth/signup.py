8ff5548c84e8b7150cf1de1bf3897533d2886e88
Vincent Revard <Vincent.Revard@gmail.com>
2024-04-25 02:32:36 -0700

    Fix signup for redis auth

--- server/routes/auth/signup.py
+++ server/routes/auth/signup.py
@@ -37,10 +37,9 @@ class Signup(Resource):
             ipdb.set_trace()
             db.session.commit()
             ipdb.set_trace()
-
             access_token = create_access_token(identity=user.id, fresh=True)
             refresh_token = create_refresh_token(identity=user.id)
-            csrf_token = generate_csrf_token()  # You'll need to implement this function
+            csrf_token = generate_csrf_token()
             user_session = {
                 "user_id": user.id,
                 "access_token": access_token,
@@ -48,17 +47,12 @@ class Signup(Resource):
                 "csrf_token": csrf_token,
             }
             user_session_str = json.dumps(user_session)
-
             redis_client.set(
                 access_token, user_session_str, ex=app.config["JWT_ACCESS_TOKEN_EXPIRES"]
             )
             response = make_response(user_schema.dump(user), 201)
-            # response.set_cookie("CSRF-TOKEN", csrf_token)
-            ipdb.set_trace()
             set_access_cookies(response, access_token)
-            ipdb.set_trace()
             set_refresh_cookies(response, refresh_token)
-            ipdb.set_trace()
             return response
         except Exception as e:
             db.session.rollback()
