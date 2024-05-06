7b6e902c211a14847664ce9554fded8356dd7a8f
Vincent Revard <Vincent.Revard@gmail.com>
2024-05-06 04:51:27 -0700

    update header/navigation

--- client/src/components/navigation/header.js
+++ client/src/components/navigation/header.js
@@ -1,160 +1,59 @@
 import { useState } from 'react'
-import {Link, useNavigate} from 'react-router-dom'
-import styled from 'styled-components'
-import { GiHamburgerMenu } from 'react-icons/gi'
+import { Link } from 'react-router-dom'
+import { AppBar, Toolbar, IconButton, List, ListItem, ListItemText, Drawer, ButtonBase } from '@mui/material'
+import MenuIcon from '@mui/icons-material/Menu'
 
-function Header({ user , logout}) {
+function Header({ user, logout }) {
   const [menu, setMenu] = useState(false)
+  const closeMenu = () => {
+    setMenu(false);
+  }
 
   return (
-  <StyledHeader> 
-    <NavH1>Reflector</NavH1>
-    <Menu>
-      {!menu?
-      <div onClick={() => setMenu(!menu)}>
-        <GiHamburgerMenu size={30} color="white"/> 
-      </div>:
-      <div>
-        <ControlPanel>
-          <XButton onClick={() => setMenu(!menu)}>X</XButton>
-          <ul>
+    <AppBar position="static">
+      <Toolbar>
+        <IconButton edge="start" color="inherit" aria-label="menu" onClick={() => setMenu(!menu)}>
+          <MenuIcon />
+        </IconButton>
+        <Drawer anchor="right" open={menu} onClose={() => setMenu(!menu)}>
+          <List>
             {user ? (
               <>
-                <li><StyledButton onClick={logout}>Logout</StyledButton></li>
-                <li><StyledButtonLink to='/courses/new'>New Course</StyledButtonLink></li>
-                <li><StyledButtonLink to={`/profile/${user.id}`}>Profile</StyledButtonLink></li>
-                <li><StyledButtonLink to='/notes'>Notes</StyledButtonLink></li>
-                <li><StyledButtonLink to='/courses'>Courses</StyledButtonLink></li>
-                <li><StyledButtonLink to='/references'>References</StyledButtonLink></li>
-                <li><StyledButtonLink to='/topics'>Topics</StyledButtonLink></li>
+                <ListItem>
+                  <ButtonBase onClick={logout}>
+                    <ListItemText primary="Logout" />
+                  </ButtonBase>
+                </ListItem>
+                <ListItem>
+                  <ButtonBase component={Link} to={`/profile/${user.id}`} onClick={closeMenu}>
+                    <ListItemText primary="Profile" />
+                  </ButtonBase>
+                </ListItem>
+                <ListItem>
+                  <ButtonBase component={Link} to='/courses' onClick={closeMenu}>
+                    <ListItemText primary="Courses" />
+                  </ButtonBase>
+                </ListItem>
               </>
             ) : (
               <>
-                <li><StyledButtonLink to='/registration'>Login/Register</StyledButtonLink></li>
+                <ListItem>
+                  <ButtonBase component={Link} to='/registration' onClick={closeMenu}>
+                    <ListItemText primary="Login/Register" />
+                  </ButtonBase>
+                </ListItem>
               </>
             )}
-            <li><StyledButtonLink to='/'> Home </StyledButtonLink></li>
-          </ul>
-        </ControlPanel>
-      </div>
-      }
-    </Menu>
-  </StyledHeader>
-    )
-  }
-  
+            <ListItem>
+              <ButtonBase component={Link} to='/' onClick={closeMenu}>
+                <ListItemText primary="Home" />
+              </ButtonBase>
+            </ListItem>
+          </List>
+        </Drawer>
+      </Toolbar>
+    </AppBar>
+  )
+}
+
 export default Header
-
-const StyledHeader = styled.div`
-  display: flex;
-  justify-content: space-between;
-  background-color: var(--header);
-  padding: 10px;
-  border-radius: 4px;
-  position: relative;
-`;
-
-const ControlPanel = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-end;
-  background-color: #D3D3D3;
-  padding: 10px;
-  border-radius: 4px;
-  position: absolute;
-  right: 0;
-  width: 120px; 
-  color: #333; // Dark gray text
-
-`;
-
-const Menu = styled.div`
-  display: flex;
-  align-items: center;
-  color: var(--header);
-  div {
-    svg {
-      color: #00008B; /* Dark blue */
-    }
-  }
-  ul {
-    background-color: #8EAOAE; /* Light gray */
-    padding: 10px;
-    border-radius: 4px;
-    list-style: none;
-  }
-  a {
-    color: #343a40; /* Dark gray */
-    text-decoration: none;
-    &:hover {
-      color: #6F8F8F; /* Blue */
-    }
-  }
-`;
-
-  const NavH1 = styled.h1`
-  font-family: 'Arial', sans-serif;
-  color: var(--bg);
-  background-color: var(--header);
-`;
-
-const XButton = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: #8EAOAE; 
-  color: #000; 
-  font-family: Arial, sans-serif;
-  font-weight: bold;
-  border: none;
-  border-radius: 50%;
-  width: 30px;
-  height: 30px;
-  cursor: pointer;
-  transition: background-color 0.3s ease;
-  margin-bottom: 10px;
-  &:hover {
-    background-color: #fff; 
-  }
-`;
-const StyledButton = styled.button`
-  display: block;
-  cursor: pointer;
-  background-color: var(--link);
-  color: var(--bg);
-  font-family: Arial, sans-serif;
-  font-weight: normal;
-  text-decoration: none;
-  border: none;
-  border-radius: 4px;
-  padding: 10px 20px;
-  font-size: 1em;
-  transition: background-color 0.3s ease;
-  outline: none;
-  margin-top: 10px;
-  &:hover, &:focus {
-    background-color: darken(var(--link), 10%);
-    color: var(--bg);
-  }
-`
-
-const StyledButtonLink = styled(Link)`
-  display: block;
-  cursor: pointer;
-  background-color: var(--link);
-  color: var(--bg);
-  font-family: Arial, sans-serif;
-  font-weight: normal;
-  text-decoration: none;
-  border: none;
-  border-radius: 4px;
-  padding: 10px 20px;
-  font-size: 1em;
-  transition: background-color 0.3s ease;
-  outline: none;
-  margin-top: 10px;
-  &:hover, &:focus {
-    background-color: darken(var(--link), 10%);
-    color: var(--bg);
-  }
-`;
