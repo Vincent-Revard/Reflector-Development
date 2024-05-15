c1e0f24d67ab8def6eefb22efd831e0f9a045c39
Vincent Revard <Vincent.Revard@gmail.com>
2024-05-15 00:37:12 -0700

    update profile form and detail ui

--- client/src/components/profile/user_profile_detail.js
+++ client/src/components/profile/user_profile_detail.js
@@ -1,233 +1,194 @@
+import { useProviderContext } from '../../context/ContextProvider';
 import React, { useState, useEffect } from 'react';
+import { useParams, useNavigate, useLocation } from 'react-router-dom';
+import { Formik, Form, Field } from 'formik';
 import * as yup from 'yup';
-import FormComponent from '../form/form_component';
-import { Formik } from 'formik'
-import Modal from 'react-modal';
+import { Grid, Button, TextField, FormControlLabel, Switch } from '@mui/material';
 
-const UserProfileDetail = ({ data, handlePatchContext, handleDeleteContext, showToast }) => {
-  const { username, email } = data;
-  const [isEditMode, setIsEditMode] = useState(false);
-  const [showChangePassword, setShowChangePassword] = useState(false);
-  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [validationSchema, setValidationSchema] = useState(null);
-  const initialFieldInfo = [
-    { name: 'username', type: 'text', placeholder: 'Username', editable: true },
-    { name: 'email', type: 'text', placeholder: 'Email', editable: true },
-  ];
-  const [fieldInfo, setFieldInfo] = useState(initialFieldInfo);
+const UserProfileDetail = ({ data }) => {
+  const { handlePatchContext, showToast } = useProviderContext();
+  const [editEnabled, setEditEnabled] = useState(false);
+  const [passwordEditEnabled, setPasswordEditEnabled] = useState(false);
 
+  const [initialValues, setInitialValues] = useState({
+    username: data?.username,
+    email: data?.email,
+    current_password: '',
+  });
 
-  const handleOpenModal = () => {
-    setIsModalOpen(true);
-  };
-
-  const handleCloseModal = () => {
-    setIsModalOpen(false);
-    cancelEdit()
-  };
-
-  const openDeleteModal = () => {
-    setIsDeleteModalOpen(true);
-  }
-
-  const closeDeleteModal = () => {
-    setIsDeleteModalOpen(false);
-  }
-
-  const handleDelete = () => {
-    handleDeleteContext();
-    closeDeleteModal();
-  }
-
-
-  const toggleEditable = (fieldName) => {
-    setFieldInfo(fieldInfo.map(field =>
-      field.name === fieldName
-        ? { ...field, editable: !field.editable }
-        : field
-    ))
-  }
-  const toggleEditMode = (isPasswordUpdate) => {
-    setIsEditMode(true);
-    setShowChangePassword(isPasswordUpdate);
-    if (isPasswordUpdate) {
-      setFieldInfo([
-        { name: 'new_password', type: 'password', placeholder: 'New Password', autoComplete: 'new-password', label: 'New Password', editable: false },
-        { name: 'current_password', type: 'password', placeholder: 'Current Password', autoComplete: 'current-password', label: 'Current Password', editable: true },
-      ]);
-      setValidationSchema(yup.object().shape({
-        current_password: yup.string().required('Please enter your current password'),
-        new_password: yup.string().required('Please enter a new password'),
-      }));
-    } else {
-      setFieldInfo([
-        { name: 'username', type: 'text', placeholder: 'Username', label: `Update Username (current: ${username})`, editable: false },
-        { name: 'email', type: 'text', placeholder: 'Email', label: `Update Email Address (current: ${email})`, editable: false },
-        { name: 'current_password', type: 'password', placeholder: 'Current Password', label: 'Current Password', editable: true },
-      ]);
-      setValidationSchema(yup.object().shape({
-        username: yup.string().required('Please enter a username'),
-        email: yup.string().email().required('Please enter an email'),
-        current_password: yup.string().required('Please enter your current password'),
-      }));
-    }
-  }
-
-  const handleError = (error) => {
-    if (typeof error === 'string') {
-      showToast('error', error);
-    } else if (error && typeof error.message === 'string') {
-      showToast('error', error.message);
-    } else if (typeof error === 'object' && error !== null) {
-      for (let field in error) {
-        error[field].forEach((message) => {
-          showToast('error', `${field}: ${message}`);
-        });
-      }
-    }
-  }
-
-  const [formValues, setFormValues] = useState({
-    username: username || '',
-    email: email || '',
+  const passwordInitialValues = {
     current_password: '',
     new_password: '',
-  });
-  useEffect(() => {
-    setFieldInfo([
-      { name: 'username', type: 'text', placeholder: 'Username', editable: false, label: `Update Username (current: ${formValues.username})` },
-      { name: 'email', type: 'text', placeholder: 'Email', editable: false, label: `Update Email Address (current: ${formValues.email})` },
-      { name: 'current_password', type: 'password', placeholder: 'Current Password', label: 'Current Password', editable: true },
-    ]);
-  }, [formValues]);
+  };
 
-  const onSubmit = (values, { setSubmitting, resetForm, setStatus }) => {
-    if (showChangePassword && !values.new_password) {
-      showToast('error', 'Please enter a new password');
-      return;
-    }
-    const payload = {
-      username: values.username,
-      email: values.email,
-      current_password: values.current_password,
+  const validationSchema = yup.object({
+    username: yup.string().required("Please enter a username"),
+    email: yup.string().email().required("Please enter an email"),
+    current_password: yup.string().required("Please enter your current password"),
+  });
+
+  const passwordValidationSchema = yup.object({
+    current_password: yup.string().required("Please enter your current password"),
+    new_password: yup.string().required("Please enter your new password"),
+  });
+
+  const onSubmit = (values, { setSubmitting }) => {
+    const dataToSend = {
+      ...values,
+      username: initialValues.username,
     };
 
-    if (showChangePassword) {
-      payload.password = values.new_password;
-    }
-    handlePatchContext(payload)
-      .then((res) => {
-        if (!res.ok) {
-          throw new Error('Update failed');
-        }
-        showToast('success', showChangePassword ? 'Password updated successfully' : 'Profile updated successfully');
-        setIsEditMode(false);
-        setShowChangePassword(false);
-        setFormValues(prevState => ({
-          username: showChangePassword ? prevState.username : res.data.username,
-          email: showChangePassword ? prevState.email : res.data.email,
-          current_password: '',
-        }), () => {
-          resetForm({
-            values: {
-              ...values,
-              current_password: '',
-              new_password: '',
-            },
-          });
-        });
+    handlePatchContext(dataToSend)
+      .then((response) => {
+        showToast('success', 'Profile updated successfully');
+        setEditEnabled(false);
       })
       .catch((error) => {
-        handleError(error);
-        setFormValues({
-          username: username || '',
-          email: email || '',
-          current_password: '',
-          new_password: '',
-        });
+        showToast('error', error.message);
       })
       .finally(() => {
         setSubmitting(false);
-        setIsModalOpen(false);
       });
   }
 
-  const cancelEdit = () => {
-    setIsEditMode(false);
-    setShowChangePassword(false);
-    setFieldInfo(initialFieldInfo);
-    setFormValues({
-      username: username || '',
-      email: email || '',
-      current_password: '',
-      new_password: '',
-    });
+  const onPasswordSubmit = (values, { setSubmitting }) => {
+    const dataToSend = {
+      username: initialValues.username,
+      email: initialValues.email,
+      current_password: values.current_password,
+      password: values.new_password,
+    };
+
+    handlePatchContext(dataToSend)
+      .then((response) => {
+        showToast('success', 'Password updated successfully');
+      })
+      .catch((error) => {
+        showToast('error', error.message);
+      })
+      .finally(() => {
+        setSubmitting(false);
+      });
   }
+
   return (
     <>
-      <button onClick={() => { toggleEditMode(false); handleOpenModal() }}>
-        Update Profile
-      </button>
-      <button onClick={() => { toggleEditMode(true); handleOpenModal(); }}>
-        Update Password
-      </button>
-      {isEditMode && (
+      {!passwordEditEnabled && (
         <Formik
-          initialValues={formValues}
+          initialValues={initialValues}
           validationSchema={validationSchema}
           onSubmit={onSubmit}
-          enableReinitialize
         >
-          {({ isSubmitting }) => (
-            <FormComponent
-              isOpen={isModalOpen}
-              onRequestClose={handleCloseModal}
-              fieldInfo={fieldInfo}
-              isSubmitting={isSubmitting}
-              cancelEdit={cancelEdit}
-              toggleEditable={toggleEditable}
-            />
+          {({ values, errors, touched, setFieldValue, isSubmitting }) => (
+            <Form>
+              <Grid container spacing={2}>
+                <Grid item xs={12}>
+                  <TextField
+                    id="username"
+                    name="username"
+                    type="text"
+                    label="Username"
+                    value={values.username}
+                    onChange={event => setFieldValue('username', event.target.value)}
+                    error={touched.username && Boolean(errors.username)}
+                    helperText={touched.username && errors.username}
+                    fullWidth
+                    disabled={!editEnabled}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    id="email"
+                    name="email"
+                    type="text"
+                    label="Email"
+                    value={values.email}
+                    onChange={event => setFieldValue('email', event.target.value)}
+                    error={touched.email && Boolean(errors.email)}
+                    helperText={touched.email && errors.email}
+                    fullWidth
+                    disabled={!editEnabled}
+                  />
+                </Grid>
+                {editEnabled && (
+                  <Grid item xs={12}>
+                    <TextField
+                      id="current_password"
+                      name="current_password"
+                      type="password"
+                      label="Current Password"
+                      value={values.current_password}
+                      onChange={event => setFieldValue('current_password', event.target.value)}
+                      error={touched.current_password && Boolean(errors.current_password)}
+                      helperText={touched.current_password && errors.current_password}
+                      fullWidth
+                    />
+                  </Grid>
+                )}
+                <Grid item xs={12}>
+                  <Button variant="contained" color="primary" onClick={() => setEditEnabled(!editEnabled)}>
+                    {editEnabled ? 'Disable Edit' : 'Enable Edit'}
+                  </Button>
+                </Grid>
+                {editEnabled && (
+                  <Grid item xs={12}>
+                    <Button variant="contained" color="primary" type="submit" disabled={isSubmitting}>
+                      Update
+                    </Button>
+                  </Grid>
+                )}
+              </Grid>
+            </Form>
           )}
         </Formik>
       )}
-      {!isEditMode && (
-        <>
-          <ul>
-            <li>Username: {username}</li>
-            <li>Email: {email}</li>
-          </ul>
-          <button onClick={openDeleteModal}>
-            Delete Profile
-          </button>
-          <Modal
-            isOpen={isDeleteModalOpen}
-            onRequestClose={closeDeleteModal}
-            contentLabel="Delete Profile Confirmation"
-          // style={{
-          //   overlay: {
-          //     backgroundColor: 'rgba(0, 0, 0, 0.5)', // semi-transparent background
-          //   },
-          //   content: {
-          //     top: '50%',
-          //     left: '50%',
-          //     right: 'auto',
-          //     bottom: 'auto',
-          //     marginRight: '-50%',
-          //     transform: 'translate(-50%, -50%)',
-          //     width: '80%', // adjust width
-          //     height: '80%', // adjust height
-          //   },
-          // }}
-          >
-            <h2>Are you sure you want to delete your profile?</h2>
-            <button onClick={handleDelete}>Yes, delete my profile</button>
-            <button onClick={closeDeleteModal}>No, keep my profile</button>
-          </Modal>
-        </>
+      <Button variant="contained" color="secondary" onClick={() => setPasswordEditEnabled(!passwordEditEnabled)}>
+        {passwordEditEnabled ? 'Hide Password Update' : 'Show Password Update'}
+      </Button>
+      {passwordEditEnabled && (
+        <Formik
+          initialValues={passwordInitialValues}
+          validationSchema={passwordValidationSchema}
+          onSubmit={onPasswordSubmit}
+        >
+          {({ values, errors, touched, setFieldValue, isSubmitting }) => (
+            <Form>
+              <Grid item xs={12}>
+                <TextField
+                  id="current_password"
+                  name="current_password"
+                  type="password"
+                  label="Current Password"
+                  value={values.current_password}
+                  onChange={event => setFieldValue('current_password', event.target.value)}
+                  error={touched.current_password && Boolean(errors.current_password)}
+                  helperText={touched.current_password && errors.current_password}
+                  fullWidth
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  id="new_password"
+                  name="new_password"
+                  type="password"
+                  label="New Password"
+                  value={values.new_password}
+                  onChange={event => setFieldValue('new_password', event.target.value)}
+                  error={touched.new_password && Boolean(errors.new_password)}
+                  helperText={touched.new_password && errors.new_password}
+                  fullWidth
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <Button variant="contained" color="primary" type="submit" disabled={isSubmitting}>
+                  Update
+                </Button>
+              </Grid>
+            </Form>
+          )}
+        </Formik>
       )}
     </>
   );
 }
-
 export default UserProfileDetail;
