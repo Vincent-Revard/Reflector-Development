57c74f27d3ef7bc1bdd636059c9c55293e9a4259
Vincent Revard <Vincent.Revard@gmail.com>
2024-05-20 07:55:45 -0700

    fix enrollment confirmation process

--- client/src/context/search_or_add_course_and_topic.js
+++ client/src/context/search_or_add_course_and_topic.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { TextField, Button, Box, Typography, List, ListItem, ListItemText, ListItemSecondaryAction, Pagination, ButtonBase } from '@mui/material';
+import { TextField, Button, Box, Typography, List, ListItem, ListItemText, ListItemSecondaryAction, Pagination, ButtonBase, Autocomplete, Modal } from '@mui/material';
 import { useProviderContext } from './ContextProvider';
 import { Link } from 'react-router-dom';
 
@@ -44,6 +44,8 @@ const SearchAndAddCourseOrTopic = ({ allNames, type, enrolledCourses, courseId, 
     const [enrollingTopics, setEnrollingTopics] = React.useState([]);
     const [enrollmentStatus, setEnrollmentStatus] = useEnrollmentStatus(isUnenrollPage, type, enrolledCourses, associatedTopics, allNames);
     const [filteredItems, setFilteredItems] = useFilteredItems(isUnenrollPage, type, enrolledCourses, associatedTopics, allNames, searchTerm);
+    const [openModal, setOpenModal] = React.useState(false);
+
 
     const handleChange = event => {
         setSearchTerm(event.target.value);
@@ -66,9 +68,12 @@ const SearchAndAddCourseOrTopic = ({ allNames, type, enrolledCourses, courseId, 
             }
         }
     };
-
     const handleClick = (id) => (event) => {
         event.preventDefault();
+        setOpenModal(true); // Open the modal when the button is clicked
+    };
+
+    const handleConfirm = (id) => {
         let topicId = null;
         let courseIdForAction = parseInt(id, 10);
 
@@ -85,10 +90,18 @@ const SearchAndAddCourseOrTopic = ({ allNames, type, enrolledCourses, courseId, 
                 showToast('success', `Successfully ${actionMessage} in ${type}`);
                 setEnrollmentStatus(prevState => ({ ...prevState, [id]: !isUnenrollPage }));
                 setFilteredItems(filteredItems.filter(item => item.id !== id));
+                setSelectedId(null); // Clear the selection
+                setSelectedName(null); // Clear the selection
+                setOpenModal(false);
             })
             .catch(error => {
                 showToast('error', `Error: ${error.message}`);
+                setOpenModal(false);
             });
+    };
+
+    const handleCancel = () => {
+        setOpenModal(false); 
     };
 
     let actionText;
@@ -101,81 +114,103 @@ const SearchAndAddCourseOrTopic = ({ allNames, type, enrolledCourses, courseId, 
     const itemsOnPage = filteredItems?.slice((page - 1) * ITEMS_PER_PAGE, page * ITEMS_PER_PAGE);
 
     return (
-        <Box sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-            <Typography variant="h4" gutterBottom>
-                {actionText}
-            </Typography>
-            <TextField
-                fullWidth
-                variant="outlined"
-                placeholder={`Search for a ${type}`}
-                value={searchTerm}
-                onChange={handleChange}
-            />
-            {type === 'topics' && courseId && !isUnenrollPage && (
-                <Box sx={{ mt: 3 }}>
-                    <Link to={`/course/${courseId}/topic/new`}>
-                        <Button variant="contained" color="primary">
-                            Create New Topic
-                        </Button>
-                    </Link>
-                </Box>
-            )}
-            {type === 'courses' && !isUnenrollPage && (
-                <Box sx={{ mt: 3 }}>
-                    <Link to="/course/new">
-                        <Button variant="contained" color="primary">
-                            Create New Course
-                        </Button>
-                    </Link>
-                </Box>
-            )}
-            {/* <List>
-                {itemsOnPage?.map(item => (
-                    <ListItem key={item.id} component="button" onClick={() => handleSelect(item.id, item.name)}>
-                        <ListItemText primary={item.name} sx={{ flexGrow: 1 }} />
-                        <ListItemSecondaryAction sx={{ flexShrink: 1 }}>
-                            {!isUnenrollPage && <Button variant="contained" color="primary" onClick={handleClick(item.id)}>Enroll</Button>}
-                            {isUnenrollPage && <Button variant="contained" color="secondary" onClick={handleClick(item.id)}>Unenroll</Button>}
-                        </ListItemSecondaryAction>
-                    </ListItem>
-                ))}
-            </List>
-            {selectedId && (
-                <Typography variant="h6">
-                    Selected {type} name: {selectedName}
+        <Box sx={{ display: 'flex', flexDirection: 'row', width: '100%', maxWidth: 720, bgcolor: 'background.paper' }}>
+            <Box sx={{ width: '50%', pr: 2 }}>
+                <Typography variant="h4" gutterBottom>
+                    {actionText}
                 </Typography>
-            )} */}
-            <List>
-                {itemsOnPage?.map(item => (
-                    <ListItem key={item.id}>
-                        <ButtonBase onClick={() => handleSelect(item.id, item.name)}>
-                            <ListItemText primary={item.name} />
-                        </ButtonBase>
-                    </ListItem>
-                ))}
-            </List>
-            {selectedId && (
-                <Box sx={{ display: 'flex', alignItems: 'center', mt: 2 }}>
-                    <Typography variant="h6" sx={{ mr: 2 }}>
-                        Selected {type} name: {selectedName}
-                    </Typography>
-                    {!isUnenrollPage && <Button variant="contained" color="primary" onClick={handleClick(selectedId)}>Enroll</Button>}
-                    {isUnenrollPage && <Button variant="contained" color="secondary" onClick={handleClick(selectedId)}>Unenroll</Button>}
+                <Autocomplete
+                    fullWidth
+                    options={allNames.map((option) => option.name)}
+                    renderInput={(params) => <TextField {...params} variant="outlined" placeholder={`Search for a ${type}`} />}
+                    onInputChange={(event, newInputValue) => {
+                        setSearchTerm(newInputValue);
+                    }}
+                />
+                {type === 'topics' && courseId && !isUnenrollPage && (
+                    <Box sx={{ mt: 3 }}>
+                        <Link to={`/course/${courseId}/topic/new`}>
+                            <Button variant="contained" color="primary">
+                                Create New Topic
+                            </Button>
+                        </Link>
+                    </Box>
+                )}
+                {type === 'topics' && (
+                    <Box sx={{ mt: 3, display: 'flex', gap: 2 }}>
+                        <Link to={`/course/${courseId}`}>
+                            <Button variant="outlined">
+                                Back to this course
+                            </Button>
+                        </Link>
+                        <Link to="/course">
+                            <Button variant="outlined">
+                                Back to Courses
+                            </Button>
+                        </Link>
+                    </Box>
+                )}
+                {type === 'courses' && !isUnenrollPage && (
+                    <Box sx={{ mt: 3 }}>
+                        <Link to="/course/new">
+                            <Button variant="contained" color="primary">
+                                Create New Course
+                            </Button>
+                        </Link>
+                    </Box>
+                )}
+                {type === 'courses' && (
+                    <Box sx={{ mt: 3, display: 'flex', gap: 2 }}>
+                        <Link to={`/course/${courseId}`}>
+                            <Button variant="outlined">
+                                Back to this course
+                            </Button>
+                        </Link>
+                        <Link to="/course">
+                            <Button variant="outlined">
+                                Back to Courses
+                            </Button>
+                        </Link>
+                    </Box>
+                )}
+                <List>
+                    {itemsOnPage?.map(item => (
+                        <ListItem key={item.id}>
+                            <ButtonBase onClick={() => handleSelect(item.id, item.name)}>
+                                <ListItemText primary={item.name} />
+                            </ButtonBase>
+                        </ListItem>
+                    ))}
+                </List>
+                <Box sx={{ mt: 3 }}>
+                    <Pagination count={Math.ceil((filteredItems?.length || 0) / ITEMS_PER_PAGE)} page={page} onChange={(event, value) => setPage(value)} />
                 </Box>
-            )}
-            <Box sx={{ mt: 3 }}>
-                <Link to="/course">
-                    <Button variant="outlined">
-                        Back to Courses
-                    </Button>
-                </Link>
             </Box>
-            <Box sx={{ mt: 3 }}>
-                <Pagination count={Math.ceil((filteredItems?.length || 0) / ITEMS_PER_PAGE)} page={page} onChange={(event, value) => setPage(value)} />
+            <Box sx={{ width: '50%', pl: 2 }}>
+                {selectedId && (
+                    <Box sx={{ display: 'flex', alignItems: 'center', mt: 2 }}>
+                        <Typography variant="h6" sx={{ mr: 2 }}>
+                            Selected {type} name: {selectedName}
+                        </Typography>
+                        {!isUnenrollPage && <Button variant="contained" color="primary" onClick={handleClick(selectedId)}>Enroll</Button>}
+                        {isUnenrollPage && <Button variant="contained" color="secondary" onClick={handleClick(selectedId)}>Unenroll</Button>}
+                    </Box>
+                )}
             </Box>
-        </Box>
+            <Modal
+                open={openModal}
+                onClose={handleCancel}
+            >
+                <Box sx={{ width: 400, padding: 2, bgcolor: 'background.paper', margin: 'auto' }}>
+                    <Typography variant="h6" gutterBottom>
+                        Are you sure you want to {isUnenrollPage ? 'unenroll from' : 'enroll in'} this {type}?
+                    </Typography>
+                    <Button variant="contained" color="primary" onClick={() => { handleConfirm(selectedId); setOpenModal(false); }}>Yes</Button>
+                    <Button variant="outlined" color="secondary" onClick={() => { handleCancel(); setOpenModal(false); }}>No</Button>
+                </Box>
+            </Modal>
+        </Box >
     );
-};
+}
 
 export default SearchAndAddCourseOrTopic;
